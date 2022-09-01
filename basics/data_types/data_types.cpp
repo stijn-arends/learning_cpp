e39be827c7c2bb89bfa48f202c490f10c514f225
stijn-arends <stijnarends@live.nl>
2022-09-01 12:39:15 +0200

    Learned about booleans and strings

--- basics/data_types/data_types.cpp
+++ basics/data_types/data_types.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <climits>
+#include <float.h>
+#include <string>
 
 using std::cout;
+
+// To se a global constant
+#define Z 10
 
 
 void integral_data_type(){
@@ -47,10 +52,72 @@ void char_data_type(){
     cout << "ASCII number for A = " << (int) a << std::endl;
 }
 
+void bool_data_types(){
+
+    bool pizza_is_good = true;
+    bool cale_is_bad = 1; // 1 equals true
+    bool sprouts_are_good = 0; // 0 equals false
+    
+    // booleans can be used as flags
+    bool found = false; // set to true when found
+
+    // In order to print a boolean as true or false you need to use std::boolalpha
+    cout << found << std::endl; // prints 0
+    cout << std::boolalpha << found << std::endl; // prints false
+
+}
+
+void float_data_types(){
+
+    float a = 10.0 / 3;
+    a = a * 100000000000;
+    double b = 7.7E4; // = 77000 or 7.7 x 10^4
+    long double c;
+
+    cout << std::fixed << a <<std::endl;
+    // this displays how mnay digits a long double can display
+    cout << LDBL_DIG << std::endl; 
+
+    // You should normally use a double
+}
+
+void const_macro_enum(){
+
+    // This is a constant and is read only we can't change the variable value
+    const int x = 5;
+
+    // enums are als constants
+    enum {y = 100}; // can't change y
+
+}
+
+void string_data_type(){
+
+    std::string greeting = "Hello";
+    // concat
+    std::string complete_greeting = greeting = " there";
+    // append
+    complete_greeting += "!";
+
+    cout << complete_greeting << std::endl;
+    cout << "Length of string: " << complete_greeting.length() << std::endl;
+
+    // method == member - function == functions attachted to objects
+
+    // How to get user input for strings:
+    std::string user_in;
+    getline(std::cin, user_in);
+    cout << "This was the user input: " << user_in << std::endl;
+}
+
 int main(){
     // There are several data types in c++
     // integral_data_type();
 
-    char_data_type();
+    // char_data_type();
+
+    // bool_data_types();
+
+    string_data_type();
 
 }
