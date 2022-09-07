84b0fcb665447d4e070403cf7cd9de6df25a9005
stijn-arends <stijnarends@live.nl>
2022-09-07 16:35:56 +0200

    Moved some of the code to the intermediate section

---
+++ intermediate/namespaces/namespace_using_multifiles/math_stuff.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+// Include the header file "" means to search in current directory
+#include "math_utils.h"
+
+int main(){
+    /* 
+    compile these scripts by using: g++ math_stuff.cpp math_utls.cpp
+    
+    */ 
+    std::cout << utils::pow(3, 2) << "\n";
+    std::cout << utils::pow(3) << "\n";
+
+    // using overload for shapes -> rectangle and square
+    utils::Rectangle rectangle;
+    rectangle.length = 10;
+    rectangle.width = 10;
+    std::cout << utils::area(rectangle.length, rectangle.width) << "\n";
+    return 0;
+}
---
+++ intermediate/namespaces/namespace_using_multifiles/math_utils.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+// Include the header file "" means to search in current directory
+#include "math_utils.h"
+
+namespace utils{
+    // for rectangle
+    double area(double length, double width){
+        return length * width;
+    }
+
+    // for square
+    double area(double length){
+        return length * length;
+    }
+
+    double area(Rectangle rectangle){
+        return rectangle.length * rectangle.width;
+    }
+
+    double pow(double base, int pow){
+
+        double total = 1;
+        for (int i = 0; i < pow; i++){
+            total *= base;
+        }
+        return total;
+    }
+}
+
